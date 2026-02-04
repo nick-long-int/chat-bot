9859d1d98e7b9d2a23e3aca81a0f99b2a82ff22c
nick-long-int <gnidenkonn@yandex.ru>
2026-02-04 23:04:42 +0300

    init eureka-server

---
+++ eureka-server/src/main/java/ru/gnidenko/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.gnidenko.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
