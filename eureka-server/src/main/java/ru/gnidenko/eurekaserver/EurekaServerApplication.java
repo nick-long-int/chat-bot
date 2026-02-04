5f5b0a192ba8eeeff955046eea4889b1e37758eb
nick-long-int <gnidenkonn@yandex.ru>
2026-02-04 23:44:38 +0300

    FIX annotation

--- eureka-server/src/main/java/ru/gnidenko/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/gnidenko/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,10 @@ package ru.gnidenko.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
