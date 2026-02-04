9de75bbde29a44547a98f60f73a65756abc3ae81
nick-long-int <gnidenkonn@yandex.ru>
2026-02-04 23:07:18 +0300

    Настройка eureka-server

--- eureka-server/src/main/java/ru/gnidenko/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/gnidenko/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package ru.gnidenko.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
